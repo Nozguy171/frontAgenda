2cef137be2b2b41f70f41cdfe5a89f896d2101b3
Bryan Juarez Limon <bryan.juarez@uabc.edu.mx>
2025-07-19 20:05:52 -0700

    fix aside

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,11 @@
+// frontend/src/app/head.tsx
 export default function Head() {
   return (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, viewport-fit=cover"
+      />
     </>
   )
 }
